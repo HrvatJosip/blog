8e7fdeb9d21c252cc00d7503acc86dc82d16e42d
Josip Dorvak <jdorvak@gmail.com>
2015-12-18 21:54:46 -0500

    Added Redcarpet & Rouge.rb for Markdown & Syntax Highlighting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,27 @@
+require 'rouge'
+require 'rouge/plugins/redcarpet'
 module ApplicationHelper
+  class HTMLwithRouge < Redcarpet::Render::HTML
+    include Rouge::Plugins::Redcarpet
+    def rouge_formatter(opts={})
+      opts ={
+        wrap: true,
+      }
+      Rouge::Formatters::HTML.new(opts)
+    end
+  end
+
+  def markdown(content)
+    renderer = HTMLwithRouge.new(hard_wrap: true, filter_html: true)
+    options = {
+      autolink: true,
+      no_intra_emphasis: true,
+      disable_indented_code_blocks: true,
+      fenced_code_blocks: true,
+      lax_html_blocks: true,
+      strikethorugh: true,
+      superscript: true
+    }
+    Redcarpet::Markdown.new(renderer, options).render(content).html_safe
+  end
 end
